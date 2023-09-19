6b740e3f738d22801da48d6e4bf0c2a56358ac84
Julesntizimira <ntizimijules5@gmail.com>
2023-09-19 22:20:35 +0000

    task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 
-class Amenity(BaseModel):
-    name = ""
+env_val = os.environ.get('HBNB_TYPE_STORAGE')
+
+
+class Amenity(BaseModel, Base):
+    if env_val == 'db':
+        __tablename__ = "amenities"
+        name = Column(
+                String(128),
+                nullable=False)
+        place_amenities = relationship(
+                "Place",
+                secondary="place_amenity",
+                back_populates="amenities",
+                viewonly=False)
+    else:
+        name = ""
--- models/place.py
+++ models/place.py
@@ -2,15 +2,34 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.user import User
+from models.amenity import Amenity
 from models.city import City
 from models.review import Review
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
 from sqlalchemy import Float
 from sqlalchemy.orm import relationship
 import os
 
 
 env_val = os.environ.get('HBNB_TYPE_STORAGE')
+
+
+place_amenity = Table(
+        "place_amenity",
+        Base.metadata,
+        Column(
+            "place_id",
+            String(60),
+            ForeignKey("places.id"),
+            primary_key=True,
+            nullable=False),
+        Column(
+            "amenity_id",
+            String(60),
+            ForeignKey("amenities.id"),
+            primary_key=True,
+            nullable=False)
+        )
 
 
 class Place(BaseModel, Base):
@@ -46,6 +65,11 @@ class Place(BaseModel, Base):
         latitude = Column(Float)
         longitude = Column(Float)
         reviews = relationship("Review", backref="place", cascade='delete')
+        amenities = relationship(
+                "Amenity",
+                secondary=place_amenity,
+                back_populates="place_amenities",
+                viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -61,7 +85,7 @@ class Place(BaseModel, Base):
 
         @property
         def reviews(self):
-            """Returns the list of City instances with
+            """Returns the list of Review instances with
                place_id equals to the current Place.id
             """
             from models.__init__ import storage
@@ -71,3 +95,17 @@ class Place(BaseModel, Base):
                 if rev.place_id == self.id:
                     reviews_list.append(rev)
             return reviews_list
+
+        @property
+        def amenities(self):
+            """returns the list of Amenity instances
+               based on the attribute amenity_ids
+               that contains all Amenity.id linked to the Place
+            """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, amenity_obj):
+            if isinstance(amenity_obj, Amenity):
+                if amenity_obj.id not in self.amenity_ids:
+                    self.amenity_ids.append(amenity_obj.id)
