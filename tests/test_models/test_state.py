b3529c76b2339f695fb777692597cf1a515f8f6b
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-20 12:47:29 +0200

    added documentation

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -5,15 +5,15 @@ from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """tests for state class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """initialise class"""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """test name type"""
         new = self.value()
         self.assertEqual(type(new.name), str)
