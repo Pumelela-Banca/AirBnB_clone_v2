cd8090772358fd30b1ec66ae5b77147059b58a44
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-20 12:45:32 +0200

    added documentation

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -5,25 +5,25 @@ from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """Test class for reviews"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """review initialisation"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """test if id is string"""
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """test if id is string """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """test if text is string """
         new = self.value()
         self.assertEqual(type(new.text), str)
