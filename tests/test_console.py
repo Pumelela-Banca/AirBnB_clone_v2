dd84dc274a697fa089b5e2a636247faa2ceedc12
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-20 15:23:56 +0200

    added empty console file

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Test console"""
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """
+    Test for console
+    """
+    def test_do_create(self):
+        """
+        Test new d==feature from do create
+        """
+        pass
