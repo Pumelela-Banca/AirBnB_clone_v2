12b26ff92d8289e4b28e11d71593209d77da66fe
Julesntizimira <ntizimijules5@gmail.com>
2023-09-20 12:14:40 +0000

    update models to hundle task 5

--- models/amenity.py
+++ models/amenity.py
@@ -3,13 +3,18 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+import os
+
+
+env_val = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship(
-            "Place",
-            secondary="place_amenity",
-            back_populates="amenities",
-            viewonly=False)
+    if env_val == 'db':
+        place_amenities = relationship(
+                "Place",
+                secondary="place_amenity",
+                back_populates="amenities",
+                viewonly=False)
