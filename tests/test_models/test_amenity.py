9a4ec3255c6fa1d361fb1945960d07303a54f7e7
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-20 12:35:43 +0200

    added documentation

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,15 +5,15 @@ from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """Amenity tests"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """initialise class"""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Test name type"""
         new = self.value()
         self.assertEqual(type(new.name), str)
