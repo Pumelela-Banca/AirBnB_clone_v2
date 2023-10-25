97dff401d705fa0e57aeaef5e6e11ac2dd228f02
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-25 23:10:41 +0200

    added task 8

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -30,5 +30,13 @@ def city_by_states():
     return render_template('8-cities_by_states.html', states=states)
 
 
+@app.teardown_appcontext
+def shut_db(err):
+    """
+    shut db
+    """
+    storage.close()
+
+
 if __name__ == '__main__':
     app.run(app.run(host="0.0.0.0", port="5000"))
