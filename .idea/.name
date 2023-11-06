fe461ef4b68b03e3cdb8ed5c33b0fc4107de8b25
Pumelela-Banca <pumelelabanca@gmail.com>
2023-11-06 18:14:42 +0200

    creates rgz file locally from web_static

---
+++ .idea/.name
@@ -0,0 +1,1 @@
+1-hbnb_route.py
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,5 +5,21 @@ Fabric script that generates a .tgz archive from the contents of
 the web_static folder of AirBnB Clone repo, using the function do_pack
 """
 
-from Fabric
+from fabric.api import isdir
+from fabric.api import local
+from datetime import datetime
 
+
+def do_pack():
+    """
+    create tgz archive from static
+    """
+    try:
+        if isdir("versions") is False:
+            local("mkdir versions")
+        name = "versions/web_static_{}.tgz".format(
+            datetime.now().strftime("%Y%m%d%H%M%S"))
+        local(f"tar -cvzf {name} web_static")
+        return name
+    except:
+        return None
