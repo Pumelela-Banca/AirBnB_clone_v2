bb63646cb8ebfe945d5b56701857bd2f519df792
Pumelela-Banca <pumelelabanca@gmail.com>
2023-11-07 01:24:42 +0200

    distributes web servers

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""
+unpacks files inside servers
+"""
+
+
+from datetime import datetime
+from os.path import isdir
+from fabric.api import run, put, env
+from fabric.api import local
+
+
+env.hosts = ["18.206.192.143", "54.83.136.225"]
+
+
+def do_pack():
+    """
+    creates tgz archive from static folder
+    """
+    try:
+        if isdir("versions") is False:
+            local("mkdir versions")
+        name = "versions/web_static_{}.tgz".format(
+            datetime.now().strftime("%Y%m%d%H%M%S"))
+        local(f"tar -cvzf {name} web_static")
+        return name
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    unpack files into servers
+    """
+    if isdir(archive_path) is False:
+        return False
+    try:
+        file = archive_path.split("/")[-1]
+        fold = file.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run(f"mkdir -p {path}{fold}")
+        run(f"tar -xzf /tmp/{file} -C {path}{fold}")
+        run(f"rm /tmp/{file}")
+        run(f"mv {path}{fold}/web_static/* {path}{fold}/")
+        run(f"rm -rf {path}{fold}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {path}{fold}/ /data/web_static/current")
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """
+    creates and distributes an archive to your web servers
+    """
+    arch_path = do_pack()
+    if arch_path is None:
+        return False
+    return do_deploy(arch_path)
